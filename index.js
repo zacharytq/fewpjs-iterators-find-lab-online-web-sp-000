c5e6f477a03af975edda8ea402d31376cbee9e17
zacharytq <zacharytq@gmail.com>
2021-01-06 11:07:58 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let thing = array.find(function(year){
+    return year['result'] === 'W'
+  });
+  if (thing) {
+    return thing.year
+  }
+}
